899f447b0a10a88315347a8159facefd0dfaed2b
namanchandak <naman.chandak@hotmail.com>
2023-09-02 23:41:53 +0530

    commented useless function

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -37,81 +37,81 @@ const authenticatedUser = (username,password)=>{ //returns boolean
   return false;
 }
 
-regd_users.post("/login", (req,res) => {
+// regd_users.post("/login", (req,res) => {
 
-  let usr = req.body.username 
-  let pass = req.body.password
+//   let usr = req.body.username 
+//   let pass = req.body.password
 
-  if(!usr || !pass)
-  {
-    return res.status(404).json({message: "Error logging in"});
-  }
+//   if(!usr || !pass)
+//   {
+//     return res.status(404).json({message: "Error logging in"});
+//   }
 
-  if(authenticatedUser(usr,pass))
-  {
-    let token = jwt.sign({ data: pass }, 'access', { expiresIn: 60*60 });
+//   if(authenticatedUser(usr,pass))
+//   {
+//     let token = jwt.sign({ data: pass }, 'access', { expiresIn: 60*60 });
 
-  //   req.session.authorization= {
-  //     token,usr
-  // }
+//   //   req.session.authorization= {
+//   //     token,usr
+//   // }
 
-    return res.status(200).send("User successfully logged in");
-  }
-  else
-  {
-    // users.push({username: usr , password : pass})
-    return res.status(208).json({message: "Invalid Login. Check username and password"});
+//     return res.status(200).send("User successfully logged in");
+//   }
+//   else
+//   {
+//     // users.push({username: usr , password : pass})
+//     return res.status(208).json({message: "Invalid Login. Check username and password"});
     
-  }
-});
+//   }
+// });
 
 
 
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
+// regd_users.put("/auth/review/:isbn", (req, res) => {
+//   //Write your code here
   
-  let isb=req.params.isbn
-  // // 
+//   let isb=req.params.isbn
+//   // // 
   
-  let book=books[isb]
-  // console.log(isbook)
-  // let ne=req.params.delete
-  if(book)
-  {
-    let next = req.body.reviews
-    books[isb].reviews= next
-    // rev.push(next)
-    // books[isb].reviews = rev;
+//   let book=books[isb]
+//   // console.log(isbook)
+//   // let ne=req.params.delete
+//   if(book)
+//   {
+//     let next = req.body.reviews
+//     books[isb].reviews= next
+//     // rev.push(next)
+//     // books[isb].reviews = rev;
 
-    res.send( books[isb] )
+//     res.send( books[isb] )
     
-  }
-  else 
-  {
-    res.send( {message : "book not exist"} )
-  }
+//   }
+//   else 
+//   {
+//     res.send( {message : "book not exist"} )
+//   }
 
-  // return res.status(300).json({message: "Yet to be implemented"});
-});
+//   // return res.status(300).json({message: "Yet to be implemented"});
+// });
 
 
 
-regd_users.delete("/auth/review/:isbn", (req, res) => 
-{
-  let del = req.params.isbn
+// regd_users.delete("/auth/review/:isbn", (req, res) => 
+// {
+//   let del = req.params.isbn
 
-  if(books[del])
-  {
-    delete books[del].reviews
-    res.send( {message : "book review deleted"} )
-  }
-  else
-  { 
-    res.send( {message : "book not exist"} )
+//   if(books[del])
+//   {
+//     delete books[del].reviews
+//     res.send( {message : "book review deleted"} )
+//   }
+//   else
+//   { 
+//     res.send( {message : "book not exist"} )
 
-  } 
+//   } 
 
-})
+// })
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
